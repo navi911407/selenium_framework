4dc034d7c6bca297342e787336a885479689f156
Diego Bedolla <dbedollaruiz@gmail.com>
2024-06-25 10:18:55 -0600

    Adding new framework config files
    BaseTest.java is update to work with config.properties
    config.properties file added
    ConfigReader.java added to interact with config.properties
    Log.java is updated
    mainsuite.xml is added
    pom.xml is updated config
    fixing typo in testng.xml

--- src/test/java/com/apex/liverpool/config/BaseTest.java
+++ src/test/java/com/apex/liverpool/config/BaseTest.java
@@ -2,24 +2,50 @@ package com.apex.liverpool.config;
 
 import com.apex.liverpool.utils.Log;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Screenshots;
 import io.qameta.allure.testng.AllureTestNg;
+import io.qameta.allure.Attachment;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
 import static com.codeborne.selenide.Selenide.*;
+import com.apex.liverpool.utils.ConfigReader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class BaseTest extends AllureTestNg {
 
     @BeforeMethod
     public void setUp() {
         Log.info("Setting up the test environment");
-        Configuration.browserSize = "1920x1080";
-        Configuration.timeout = 10000;
+        Configuration.baseUrl = ConfigReader.getProperty("base.url");
+        Configuration.browserSize = ConfigReader.getProperty("browser.size");
+        Configuration.timeout = Integer.parseInt(ConfigReader.getProperty("global.timeout"));
+        open(Configuration.baseUrl);
+        Log.info(Configuration.baseUrl);
     }
 
     @AfterMethod
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
         Log.info("Tearing down the test environment");
+
+        if (ITestResult.FAILURE == result.getStatus()) {
+            captureScreenshot(result.getName());
+        }
+
         closeWebDriver();
+    }
+
+    @Attachment(value = "Screenshot of {0}", type = "image/png")
+    public byte[] captureScreenshot(String testName) {
+        File screenshot = Screenshots.takeScreenShotAsFile();
+        try {
+            return new FileInputStream(screenshot).readAllBytes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new byte[0];
     }
 }
